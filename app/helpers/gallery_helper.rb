d25d49c167fb2e6997a0e5585ff6a8c40a1474bd
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-03-11 15:01:30 +0700

    resize images correctly

--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -2,8 +2,9 @@
 
 module GalleryHelper
 
-  def prepare_picture_url(url)
-    url.insert(url.rindex('/'), '/700-700')
+  def galeria_link(picture)
+    image = EspCommons::Image.new(:url => picture.picture_url).parse_url
+    link_to image_tag_for(image.create_thumbnail(:width => 200, :height => 150)), image.create_thumbnail(:width => 1024, :height => 768).url
   end
 
 end
