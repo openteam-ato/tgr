ee0910528288bd4efe071352d6a4bac5c2deb8d3
Dmitry Lihachev <lda@openteam.ru>
2012-03-12 10:29:16 +0700

    force image sizes

--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -3,8 +3,8 @@
 module GalleryHelper
 
   def galeria_link(picture)
-    image = EspCommons::Image.new(:url => picture.picture_url).parse_url
-    link_to image_tag_for(image.create_thumbnail(:width => 200, :height => 150)), image.create_thumbnail(:width => 1024, :height => 768).url
+    image = EspCommons::Image.new(:url => picture.picture_url, :description => picture.description).parse_url
+    link_to image_tag_for(image.create_thumbnail(:width => 200, :height => 128, :crop => true)), image.create_thumbnail(:width => 1050, :height => 700, :crop => true).url
   end
 
 end
