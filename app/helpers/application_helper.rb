b4c40962c1210a501ea91702f4dd6dc134797cd2
Manin Iliy <bkmzvf@gmail.com>
2012-03-13 15:10:49 +0700

    #9414. отобразить навигацию третьего уровня

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,9 @@ module ApplicationHelper
   def render_navigation(hash)
     return '' if hash.nil? || hash.empty?
     content_tag :ul do
-      hash.values.first['children'].map do |key, value|
-        content_tag :li, :class => value['selected'] ? :selected : nil do
-          link_to(value['title'], value['path'], :class => key) +
-            render_navigation(value['children'] || {})
+      hash.map do |key, value|
+        content_tag :li, :class => value['selected'] ? 'selected' : nil do
+          link_to(value['title'], value['path'])+render_navigation(value['children'])
         end
       end.join.html_safe
     end
