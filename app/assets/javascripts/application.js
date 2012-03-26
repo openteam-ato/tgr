ee251da2c89c116e636bb980e0288254ce7bf3f0
Manin Iliy <bkmzvf@gmail.com>
2012-03-26 15:36:57 +0700

    collapser на странице региона

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -71,8 +71,54 @@ function banner_carousel() {
   };
 };
 
+function init_collapser() {
+  var wrapper = $('.need_collapser');
+
+  $('.collapse_item').hide();
+
+  wrapper.children('h2').wrapInner('<a class="slider" />').each(function(index, item){ $(item).attr('id', 'item_'+index) }).css('cursor', 'pointer');
+
+  wrapper.click(function(event){
+    var $this = $(event.target);
+
+    if ($this.hasClass('busy')){
+      return false;
+    }
+
+    var id = $this.parent().attr('id');
+    $this.parent().attr('id','');
+    window.location.hash = id;
+    $this.parent().attr('id', id);
+
+    if ($this.hasClass('slider')){
+      $this.addClass('busy').parent().next('.collapse_item').css('margin-top', '10px').slideToggle('slow', function(){
+        $this.toggleClass('open');
+        $this.removeClass('busy');
+        if (!$this.hasClass('open')) {
+          window.location.hash = 'main';
+        };
+      });
+      return false;
+    };
+  });
+
+};
+
+function hash_handler(){
+  var hash = window.location.hash;
+  if (hash.length > 0 && hash.match(/item/)) {
+    var target = $(hash);
+    window.scrollTo(0, target.offset().top)
+    target.children('a').click();
+  };
+};
+
 $(function() {
   init_galleria();
   slide_opener();
   banner_carousel();
+  if ($('.need_collapser').length > 0) {
+    init_collapser();
+  };
+  hash_handler();
 });
