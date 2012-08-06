2be234c7e2042c6362a75b9e94eb402575db67ae
Evgeny Lapin <lev@openteam.ru>
2012-08-06 08:50:59 +0700

    fix error

--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -4,7 +4,7 @@ module GalleryHelper
 
   def galleria_link(picture)
     image = EspCommons::Image.new(:url => picture.try(:picture_url) || picture.try(:url)).parse_url
-    return unless image.image?
+    return unless image || image.image?
     thumbnail_url = image.create_thumbnail(:width => 200, :height => 128, :crop => true).url
     image_url = image.create_thumbnail(:width => 1050, :height => 700, :crop => true).url
     link_to image_tag(thumbnail_url, :alt => nil, :title => picture.description), image_url
