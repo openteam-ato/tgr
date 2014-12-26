ee4a0f638710ce186dbd57981eae5189abc2e6d3
Evgeny Lapin <lev@openteam.ru>
2014-12-26 16:01:06 +0600

    added cap task for synchronize public/system files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
 require 'openteam/capistrano/recipes'
 
+namespace :files do
+  desc 'Synchronize your local files in public/system using remote files'
+  task :pull do
+    transfer :down, "#{shared_path}/system", 'public', :via =>:scp, :recursive =>:true
+  end
+end
+
+set :shared_children, fetch(:shared_children) + %w[public/system]
+
 set :default_stage, :ato
